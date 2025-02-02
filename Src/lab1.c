25d9aeadea070a4799363a99deb3aada4dee4e04
Slippery-Electric-Eel <jakob.travis@utah.edu>
2025-02-02 16:44:26 -0700

    Struggling with checking button output

--- Src/lab1.c
+++ Src/lab1.c
@@ -1,6 +1,6 @@
 #include <stm32f0xx_hal.h>
 #include <assert.h>
-    int lab1_main(void) {
+int lab1_main(void) {
     HAL_Init(); // Reset of all peripherals, init the Flash and Systick
     SystemClock_Config(); //Configure the system clock
     /* This example uses HAL library calls to control
@@ -12,13 +12,19 @@
     GPIO_MODE_OUTPUT_PP,
     GPIO_SPEED_FREQ_LOW,
     GPIO_NOPULL};
-    HAL_GPIO_Init(GPIOC, &initStr); // Initialize pins PC8 & PC9
+    My_HAL_GPIO_Init(GPIOC, &initStr);// Initialize pins PC8 & PC9
     HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_SET); // Start PC8 high
-    assert(GPIOC->MODER == 0 << 14);
-    assert(GPIOC-> MODER == 1 << 16);
+    //assert(GPIOC->MODER == 0 << 14);
+    GPIOA -> MODER |= 0b00;
+    GPIOA -> OSPEEDR |= 0b00;
+    GPIOA -> PUPDR |= 0b00;
+    //assert(GPIOC-> MODER == 1 << 16);
     while (1) {
         HAL_Delay(200); // Delay 200ms
         // Toggle the output state of both PC8 and PC9
-        HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_8 | GPIO_PIN_9);
+        if(!(GPIOA->IDR & 0b1)) {
+            HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_8 | GPIO_PIN_9);
+        }
+    
     }
 }
