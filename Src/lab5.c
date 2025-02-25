b92fd54d90bff56cc81284dddf05ccf8af5f7aad
Slippery-Electric-Eel <jakob.travis@utah.edu>
2025-02-25 10:45:16 -0700

    Lab 5 init ---- just wrote TXDR line

--- Src/lab5.c
+++ Src/lab5.c
@@ -32,6 +32,13 @@ int lab3_main(void) {
     My_HAL_GPIO_WritePin(GPIOC,GPIO_PIN_0, 1); //Writing PC0 and PB14 high
     My_HAL_GPIO_WritePin(GPIOB,GPIO_PIN_14,1);
 
+    //------------Initiliazing LEDs-----------------
+    GPIO_InitTypeDef initStr = {GPIO_PIN_6| GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9,
+        GPIO_MODE_OUTPUT_PP,
+        GPIO_SPEED_FREQ_LOW,
+        GPIO_NOPULL};
+    HAL_GPIO_Init(GPIOC, &initStr);
+
     //------------I2C Initiliazition--------------
     RCC -> APB1ENR |= RCC_APB1ENR_I2C2EN; //enabling the I2C2 clock in RCC peripheral
     // TIMINGR Registrer for 100kHz...SCLL = 0x13, SCLH - 0xF, SDADEL = 0x2, SCLDEL = 0x4, PRESC = 1.
@@ -44,4 +51,25 @@ int lab3_main(void) {
     I2C2 -> CR1 = 1; //enabling I2C peripheral using the PE bit in CR1
 
     //---------------Reading the Register---------------
+    //------1.--------
+    uint8_t L3addy = 0x6B;
+    I2C2 -> CR2 |= (L3addy<<1); // setiing L3GD20's slave address into the CR2 shifted by 1
+    I2C2 -> CR2 |= (1<<16);//number of bits to transmit =1
+    I2C2 -> CR2 &= ~(1<<10); //Master Requests a write transfer
+    I2C2 -> CR2 |= (1<<13); //START GENERATION!
+
+    //-----5.4------
+    while(~(I2C2->ISR & I2C_ISR_NACKF)|(I2C2->ISR & I2C_ISR_TXIS)){//if either TXIS or NACKF are not set - stay in while loop
+        if (I2C2->ISR & I2C_ISR_NACKF){
+            My_HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_7); //toggle blue LED if NACKF flag is set...slave did not respond.
+        }
+        else{
+            My_HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_6); // toggle pin 6
+            I2C2 -> CR1 |= (1<<1); //enabling TXIE
+            I2C2 -> TXDR |= (I2C2->CR2 & I2C_CR2_SADD); //Writing the address into the Transmit data repo
+
+        };
+    }
+
+
 };
